66f5c4ae25b581cc91fe4c098a6ee06919099358
tw3 <tw3coder@gmail.com>
2026-02-18 12:40:31 -0600

    add comments

--- apps/acme/src/app/configs/acme-app.config.ts
+++ apps/acme/src/app/configs/acme-app.config.ts
@@ -7,6 +7,44 @@ import {
 import { acmeAppRoutes } from '../routes/acme-app.routes';
 import { CustomPreloadingStrategy } from './custom-preloading-strategy';
 
+/**
+ * Steps:
+ * 1. Launch app: npm start
+ * 2. Create new browser tab and open Dev Tools > Console
+ * 3. Load app in tab: http://localhost:4200/
+ *    >> As expected:
+ *      - We see this console output:
+ *        [acmeAuthGuard] isAuthenticated: false
+ *      - The app redirects to http://localhost:4200/logged-out
+ * 4. Click "Login" button and observe console output
+ *    >> As expected we see this console output:
+ *       [acmeAuthGuard] isAuthenticated: true
+ *       [provideSimplePostAuthService] called
+ *       [provideSimplePostAuthService] initializer called
+ *       [SimplePostAuthService] started at 12:32:04 PM
+ *       (5 seconds later)
+ *       [provideContactUsService] called
+ *       [provideContactUsService] initializer called
+ *       [ContactUsService] constructor called
+ *       [ContactUsService] started at 12:33:50 PM
+ * 5. Click "Logout" button
+ *    >> As expected we see this console output:
+ *       [ContactUsService] destroyed
+ *       [SimplePostAuthService] destroyed
+ *    >> Unexpectedly, we also see this console output:
+ *       [provideSimplePostAuthService] initializer called
+ *       [SimplePostAuthService] started at 12:35:10 PM
+ *       [provideContactUsService] initializer called
+ *       [ContactUsService] constructor called
+ *       [ContactUsService] started at 12:35:10 PM
+ *
+ * The CustomPreloadingStrategy preloader ignores the canMatch guard and re-triggers
+ * the environment initializers under acmePostAuthRoutes
+ *
+ * If you comment out withPreloading() below the environment initializers are
+ * not re-triggered on logout
+ */
+
 export const acmeAppConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
